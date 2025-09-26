ef205e59d1acd9f326192b79466cbba502d380f8
ghatraxion <agh.arrafif@gmail.com>
2025-09-26 09:58:42 +0700

    my first game using curses:) *LOL

--- anothercurses.py
+++ anothercurses.py
@@ -8,4 +8,20 @@ def main(stdscr):
     y, x = 5, 10
 
     while True:
-        stdscr
+        stdscr.clear()
+        stdscr.addstr(y, x, "#")
+        stdscr.refresh()
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP:
+            y += 1
+        elif key == curses.KEY_DOWN:
+            y -= 1
+        elif key == curses.KEY_RIGHT:
+            x += 1
+        elif key == curses.KEY_LEFT:
+            x -= 1
+        elif key == ord("q"):
+            break
+curses.wrapper(main)
